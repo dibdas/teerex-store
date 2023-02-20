9840a147e669db9496292c40b24899567f9a5802
dibdas <dibyandu08das@gmail.com>
2023-02-20 21:12:04 +0530

    modified

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,12 +1,33 @@
 import React from "react";
 import Category from "../../components/category/Category";
 import Product from "../../components/Products/Product";
+import Search from "../../components/search/Search";
+import "./Home.scss";
 
 function home(props) {
   return (
-    <div>
-      <Category />
-      <Product />
+    <div className="home-container">
+      <div className="search-container">
+        <Search />
+      </div>
+
+      <div className="category-product-container">
+        <div className="category-container">
+          <Category />
+        </div>
+        <div className="product-container">
+          <Product />
+          <Product />
+          <Product />
+          <Product />
+          <Product />
+          <Product />
+          <Product />
+          <Product />
+          <Product />
+          <Product />
+        </div>
+      </div>
     </div>
   );
 }
