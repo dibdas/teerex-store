3bf304535d9145cb0de39c13245f04b12ff2aee2
dibdas <dibyandu08das@gmail.com>
2023-02-20 21:10:26 +0530

    modified

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 // import logo from './logo.svg';
 import "./App.css";
+import Navbar from "./components/navbar/Navbar";
+import Home from "./pages/Home/Home";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Navbar />
+      <Home />
+    </div>
+  );
 }
 
 export default App;
