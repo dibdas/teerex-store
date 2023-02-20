0e17e76c85e6fe96162f6fbceadf07f8493cadbf
dibdas <dibyandu08das@gmail.com>
2023-02-20 18:20:10 +0530

    added  home

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Category from "../../components/category/Category";
+import Product from "../../components/Products/Product";
+
+function home(props) {
+  return (
+    <div>
+      <Category />
+      <Product />
+    </div>
+  );
+}
+
+export default home;
