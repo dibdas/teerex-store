e4e0b0663cff39beeaba74358b20123a0bb83db1
dibdas <dibyandu08das@gmail.com>
2023-02-21 21:21:33 +0530

    modified

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,10 +1,23 @@
-import React from "react";
-import Category from "../../components/category/Category";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import Categories from "../../components/categories/Categories";
 import Product from "../../components/Products/Product";
 import Search from "../../components/search/Search";
+import { fetchData } from "../../redux/productSlice";
 import "./Home.scss";
 
-function home(props) {
+function Home(props) {
+  const dispatch = useDispatch();
+  const products = useSelector((state) => state?.productReducer?.products);
+  const colors = [...new Set(products?.map((item) => item.color))];
+  console.log(colors);
+  const type = [...new Set(products?.map((item) => item.type))];
+  console.log(type);
+  const gender = [...new Set(products?.map((item) => item.gender))];
+  console.log(gender);
+  useEffect(() => {
+    dispatch(fetchData());
+  }, []);
   return (
     <div className="home-container">
       <div className="search-container">
@@ -13,23 +26,16 @@ function home(props) {
 
       <div className="category-product-container">
         <div className="category-container">
-          <Category />
+          <Categories colors={colors} type={type} gender={gender} />
         </div>
         <div className="product-container">
-          <Product />
-          <Product />
-          <Product />
-          <Product />
-          <Product />
-          <Product />
-          <Product />
-          <Product />
-          <Product />
-          <Product />
+          {products?.map((item, id) => {
+            return <Product product={item} key={id} />;
+          })}
         </div>
       </div>
     </div>
   );
 }
 
-export default home;
+export default Home;
