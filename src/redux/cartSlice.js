6a852bd66a8d46735df5ceb0a10f287756ff03a8
dibdas <dibyandu08das@gmail.com>
2023-02-22 03:01:04 +0530

    modified

--- src/redux/cartSlice.js
+++ src/redux/cartSlice.js
@@ -6,19 +6,22 @@ const cartSlice = createSlice({
   },
   reducers: {
     addToCart: (state, action) => {
-      console.log(action.payload);
-      const currentItemId = action.payload;
-      const index = state.cart.findIndex((item) => item.id === currentItemId);
+      //   console.log(action.payload);
+      const currentItem = action.payload;
+      //   console.log(currentItemId);
+      const index = state.cart.findIndex((item) => item.id === currentItem.id);
       if (index === -1) {
-        state.cart.push({ ...currentItemId, quantity: 1 });
+        // console.log(state?.cart);
+        // console.log({ ...currentItem, quantity: 1 });
+        state.cart.push({ ...currentItem, quantity: 1 });
       } else {
         state.cart[index].quantity = state.cart[index].quantity + 1;
       }
     },
     deleteFromCart: (state, action) => {
-      console.log(action.payload);
-      const currentItemId = action.payload;
-      const index = state.cart.findIndex((item) => item.id === currentItemId);
+      //   console.log(action.payload);
+      const currentItem = action.payload;
+      const index = state.cart.findIndex((item) => item.id === currentItem.id);
       if (index === -1) {
         return;
       } else if (state.cart[index].quantity === 1) {
