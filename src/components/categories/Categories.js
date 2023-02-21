5b6268965bb11102eae3ba26dfdf849ca8d093ec
dibdas <dibyandu08das@gmail.com>
2023-02-22 02:58:59 +0530

    modified

--- src/components/categories/Categories.js
+++ src/components/categories/Categories.js
@@ -1,8 +1,19 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
 import "./Categories.scss";
 
 function Category({ colors, type, gender }) {
-  console.log(colors, type, gender);
+  const [types, setTypes] = useState("");
+  // console.log(colors, type, gender);
+  const products = useSelector((state) => state.productReducer.products);
+  const productsArray = products.filter((item) => item.type !== types);
+  console.log(productsArray);
+  function handleChange(event) {
+    console.log(event.target.value);
+    setTypes(event.target.value);
+  }
+
+  useEffect(() => {}, [type, gender]);
   return (
     <div>
       <section className="categories">
@@ -10,8 +21,8 @@ function Category({ colors, type, gender }) {
           <h5>Color</h5>
           {colors.map((color, id) => (
             <div className="colors" key={id}>
-              <input type="radio" value={color} id={id} />
-              <label htmlFor="">{color}</label>
+              <input type="radio" value={color} id={color} name={colors} />
+              <label htmlFor={color}>{color}</label>
             </div>
           ))}
 
@@ -22,8 +33,8 @@ function Category({ colors, type, gender }) {
           <h5>Gender</h5>
           {gender.map((item, id) => (
             <div className="gender" key={id}>
-              <input type="radio" value={item} />
-              <label htmlFor="">{item}</label>
+              <input type="radio" value={item} id={item} />
+              <label htmlFor={item}>{item}</label>
             </div>
           ))}
         </div>
@@ -31,8 +42,17 @@ function Category({ colors, type, gender }) {
           <h5>Type</h5>
           {type.map((item, id) => (
             <div className="types" key={id}>
-              <input type="radio" value={item} />
-              <label htmlFor="">{item}</label>
+              <input
+                type="radio"
+                value={item}
+                id={item}
+                name={type}
+                // checked={item}
+                onChange={handleChange}
+              />
+              <label htmlFor={item} name={type}>
+                {item}
+              </label>
             </div>
           ))}
         </div>
