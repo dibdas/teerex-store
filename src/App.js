d15f42b1fb69865b5243a2281cd88018fa47130d
dibdas <dibyandu08das@gmail.com>
2023-02-22 22:22:34 +0530

    modified

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 // import logo from './logo.svg';
-import { Route, Routes } from "react-router";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/navbar/Navbar";
+import Collections from "./pages/collections/Collections";
 import Home from "./pages/Home/Home";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route
+          path="/collections/:collectionParamsId?"
+          element={<Collections />}
+        />
+      </Routes>
     </div>
   );
 }
