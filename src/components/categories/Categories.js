6fda4cc150d6060bbffc375b3966ea27dae1ba05
dibdas <dibyandu08das@gmail.com>
2023-02-22 22:20:46 +0530

    modified

--- src/components/categories/Categories.js
+++ src/components/categories/Categories.js
@@ -1,28 +1,74 @@
 import React, { useState, useEffect } from "react";
-import { useSelector } from "react-redux";
+
 import "./Categories.scss";
+import { useNavigate } from "react-router";
 
-function Category({ colors, type, gender }) {
-  const [types, setTypes] = useState("");
+function Categories({ colors, type, gender, onTypes, onGender, onColor }) {
+  // const [types, setTypes] = useState(null);
+  // const [color, setColor] = useState(null);
+  // const [genders, setGenders] = useState(null);
+  const navigate = useNavigate();
   // console.log(colors, type, gender);
-  const products = useSelector((state) => state.productReducer.products);
-  const productsArray = products.filter((item) => item.type !== types);
-  console.log(productsArray);
-  function handleChange(event) {
-    console.log(event.target.value);
-    setTypes(event.target.value);
-  }
+  // const products = useSelector((state) => state.productReducer.products);
 
-  useEffect(() => {}, [type, gender]);
+  // function handleChangeType(event) {
+  //   console.log(event.target.value);
+
+  //   setTypes(event.target.value);
+  // }
+  // function handleChangeColor(event) {
+  //   console.log(event.target.value);
+  //   setColor(event.target.value);
+  // }
+  // function handleChangeGender(event) {
+  //   console.log(event.target.value);
+  //   setGenders(event.target.value);
+  // }
+  // console.log(color, genders, types);
+
+  // useEffect(() => {
+  //   const productsArray = products.filter((item) => item.color !== color);
+  //   console.log(productsArray);
+  // }, [color]);
+  // useEffect(() => {
+  //   const productsArray = products.filter((item) => item.type !== types);
+  //   console.log(productsArray);
+  // }, [types]);
+  // useEffect(() => {
+  //   const productsArray = products.filter((item) => item.gender !== genders);
+  //   console.log(productsArray);
+  // }, [genders]);
+
+  // useEffect(() => {
+  //   console.log(genders, color, types);
+  //   if (color !== null && genders !== null && types !== null) {
+  //     const productsArray = products.filter(
+  //       (item) =>
+  //         item.color === color && item.gender === genders && item.type === types
+  //     );
+  //     console.log(productsArray);
+  //   }
+  // }, [color, genders, types]);
+
   return (
     <div>
       <section className="categories">
         <div className="color-category">
           <h5>Color</h5>
-          {colors.map((color, id) => (
+          {colors?.map((color, id) => (
             <div className="colors" key={id}>
-              <input type="radio" value={color} id={color} name={colors} />
-              <label htmlFor={color}>{color}</label>
+              <input
+                type="radio"
+                value={color}
+                id={color}
+                name={colors}
+                // onChange={handleChangeColor}
+                onChange={() => onColor(color)}
+                // onClick={() => navigate(`/collections/${color}`)}
+              />
+              <label htmlFor={color} name={colors}>
+                {color}
+              </label>
             </div>
           ))}
 
@@ -31,16 +77,26 @@ function Category({ colors, type, gender }) {
         </div>
         <div className="gender-category">
           <h5>Gender</h5>
-          {gender.map((item, id) => (
+          {gender?.map((gen, id) => (
             <div className="gender" key={id}>
-              <input type="radio" value={item} id={item} />
-              <label htmlFor={item}>{item}</label>
+              <input
+                type="radio"
+                value={gen}
+                id={gen}
+                name={gender}
+                // onChange={handleChangeGender}
+                onChange={() => onGender(gen)}
+                // onClick={() => navigate(`/collections/${gen}`)}
+              />
+              <label htmlFor={gen} name={gender}>
+                {gen}
+              </label>
             </div>
           ))}
         </div>
         <div className="type-category">
           <h5>Type</h5>
-          {type.map((item, id) => (
+          {type?.map((item, id) => (
             <div className="types" key={id}>
               <input
                 type="radio"
@@ -48,7 +104,9 @@ function Category({ colors, type, gender }) {
                 id={item}
                 name={type}
                 // checked={item}
-                onChange={handleChange}
+                // onChange={handleChangeType}
+                onChange={() => onTypes(item)}
+                // onClick={() => navigate(`/collections/${item}`)}
               />
               <label htmlFor={item} name={type}>
                 {item}
@@ -61,4 +119,4 @@ function Category({ colors, type, gender }) {
   );
 }
 
-export default Category;
+export default Categories;
