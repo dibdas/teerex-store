81c8850d6a8dda5726dd97625dee5335d66a3ec0
dibdas <dibyandu08das@gmail.com>
2023-02-25 00:48:59 +0530

    modified

--- src/components/category/Category.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Category({ color, type }) {
-  return <div></div>;
-}
-
-export default Category;
--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -1,17 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import { BsSearch } from "react-icons/bs";
 import { FiFilter } from "react-icons/fi";
 import "./search.scss";
 
-function Search(props) {
+function Search({ onSearch }) {
+  // console.log(onSearch);
+  const [input, setInput] = useState("l");
+  console.log(input);
+  // setSearchInput(input);
+  function handleChange(event) {
+    event.preventDefault();
+    console.log(event.target.value);
+    if (event.target.value === "") {
+      setInput("Men");
+    }
+    onSearch(input);
+  }
   return (
     <div className="search">
       <div className="search-component">
         <div className="input">
-          <input type="text" placeholder="search for products" />
+          <input
+            type="text"
+            placeholder="search for products"
+            onChange={(event) => setInput(event.target.value)}
+          />
         </div>
         <div className="icon">
-          <BsSearch />
+          <BsSearch onClick={() => onSearch(input)} />
         </div>
         <div className="filter-option">
           <FiFilter />
