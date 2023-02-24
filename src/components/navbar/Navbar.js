7677c9def2e6a09f0b329bb712a174ac55a03309
dibdas <dibyandu08das@gmail.com>
2023-02-25 00:40:38 +0530

    modified

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,22 +1,29 @@
 import React from "react";
 import "./Navbar.scss";
 import { BsCart3 } from "react-icons/bs";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 function Navbar(props) {
+  const navigate = useNavigate();
+  const cart = useSelector((state) => state.cartReducer.cart);
+  let totalItem = 0;
+  cart.forEach((item) => (totalItem = totalItem + item.quantity));
+
   return (
     <div>
       <nav className="navbar">
         <div className="container">
-          <div className="nav-left">
+          <div className="nav-left" onClick={() => navigate("/")}>
             <h4>TeeRex Store</h4>
           </div>
           <div className="nav-right">
             <h4 className="products">Products</h4>
             <div className="cart-container">
-              <div className="icons">
+              <div className="icons" onClick={() => navigate("/cart")}>
                 <BsCart3 />
               </div>
-              <strong className="cart-count">99</strong>
+              <strong className="cart-count">{totalItem}</strong>
             </div>
           </div>
         </div>
