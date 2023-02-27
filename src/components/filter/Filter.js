2e3748df287903f5c72e87bc5c4465729464fe94
dibdas <dibyandu08das@gmail.com>
2023-02-27 19:39:21 +0530

    modified

--- src/components/filter/Filter.js
+++ src/components/filter/Filter.js
@@ -1,24 +1,28 @@
 import React, { useState } from "react";
+import { AiOutlineClose } from "react-icons/ai";
 import { BsSearch } from "react-icons/bs";
 import { FiFilter } from "react-icons/fi";
 import Categories from "../categories/Categories";
+import { useDispatch, useSelector } from "react-redux";
 import "./Filter.scss";
 
-function Filter() {
-  const [openFilter, setOpenFilter] = useState(false);
-  console.log(openFilter);
+function Filter({ onClose }) {
+  const [open, setOpen] = useState(false);
+  console.log(open);
   return (
     <div className="filter">
       <div className="filter-component">
-        <div
-          className="filter-option"
-          onClick={() => setOpenFilter(!openFilter)}
-        >
+        <div className="filter-option" onClick={() => onClose(false)}>
           <FiFilter />
         </div>
-        <div className="category-component">
-          <Categories />
-        </div>
+        {/* 
+        {openFilter && (
+          <div className="category-component">
+            <div className="close-open-container">
+              <Categories colors={colors} type={type} gender={gender} />
+            </div>
+          </div>
+        )} */}
       </div>
     </div>
   );
